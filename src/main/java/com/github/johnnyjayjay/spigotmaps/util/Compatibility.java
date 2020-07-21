4f80348a967754cc98256383d7a3953be83ff44a
JohnnyJayJay <johnnyjayjay02@gmail.com>
2020-07-21 17:37:12 +0200

    Indent Compatibility class with 4 spaces

--- src/main/java/com/github/johnnyjayjay/spigotmaps/util/Compatibility.java
+++ src/main/java/com/github/johnnyjayjay/spigotmaps/util/Compatibility.java
@@ -15,37 +15,37 @@ import java.util.regex.Pattern;
  */
 public final class Compatibility {
 
-  private static final boolean legacy;
-  private static final MethodHandle getId;
+    private static final boolean legacy;
+    private static final MethodHandle getId;
 
-  static {
-    Matcher versionFinder = Pattern.compile("(?<=\\(MC: )\\d+\\.\\d+\\.\\d+(?=\\))").matcher(Bukkit.getVersion());
-    if (!versionFinder.find()) {
-      throw new AssertionError("Could not find MC version in Bukkit.getVersion()");
+    static {
+        Matcher versionFinder = Pattern.compile("(?<=\\(MC: )\\d+\\.\\d+\\.\\d+(?=\\))").matcher(Bukkit.getVersion());
+        if (!versionFinder.find()) {
+            throw new AssertionError("Could not find MC version in Bukkit.getVersion()");
+        }
+        int[] version = Arrays.stream(versionFinder.group().split("\\."))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        legacy = version[0] < 1 || version[1] < 13 || version[2] < 2;
+
+        MethodType methodType = MethodType.methodType(legacy ? short.class : int.class);
+        try {
+            getId = MethodHandles.publicLookup().findVirtual(MapView.class, "getId", methodType);
+        } catch (NoSuchMethodException | IllegalAccessException e) {
+            throw new AssertionError("MapView#getId() could not be found. This should never happen.");
+        }
     }
-    int[] version = Arrays.stream(versionFinder.group().split("\\."))
-        .mapToInt(Integer::parseInt)
-        .toArray();
-    legacy = version[0] < 1 || version[1] < 13 || version[2] < 2;
 
-    MethodType methodType = MethodType.methodType(legacy ? short.class : int.class);
-    try {
-      getId = MethodHandles.publicLookup().findVirtual(MapView.class, "getId", methodType);
-    } catch (NoSuchMethodException | IllegalAccessException e) {
-      throw new AssertionError("MapView#getId() could not be found. This should never happen.");
+    public static boolean isLegacy() {
+        return legacy;
     }
-  }
 
-  public static boolean isLegacy() {
-    return legacy;
-  }
-
-  public static int getId(MapView map) {
-    try {
-      return ((Number) getId.invoke(map)).intValue();
-    } catch (Throwable throwable) {
-      throw new RuntimeException(throwable);
+    public static int getId(MapView map) {
+        try {
+            return ((Number) getId.invoke(map)).intValue();
+        } catch (Throwable throwable) {
+            throw new RuntimeException(throwable);
+        }
     }
-  }
 
 }
